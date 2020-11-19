160b62752b6e69d8598669a550b7d94aa9998963
Poujhit <poujhit112313@gmail.com>
2020-11-19 15:13:32 +0530

    web changes

--- lib/web_files/main_web.dart
+++ lib/web_files/main_web.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'web_screen/first_screen_web.dart';
@@ -5,9 +6,20 @@ import 'web_screen/first_screen_web.dart';
 class WebMainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: FirstScreen(),
-    );
+    return FutureBuilder<Object>(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting)
+            return MaterialApp(
+                home: Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ));
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: FirstScreen(),
+          );
+        });
   }
 }
